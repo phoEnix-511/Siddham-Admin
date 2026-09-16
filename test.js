3b2785f59d484e694f95a0a33f3ea6f611ea0ce6
phoenix-511 <akshay.pipre@yahoo.com>
2026-09-16 22:34:52 +0530

    Remove test files

--- test.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const subs = await prisma.adminPushSubscription.count();
-  console.log('Subscriptions:', subs);
-}
-run();
